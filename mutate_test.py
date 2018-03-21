f9ca12f5870b75ba09638ad2d3a0ffc7ec5b7cf8
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-03-21 15:52:57 -0700

    Update mutate_test.py

--- mutate_test.py
+++ mutate_test.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 15 20:59:03 2018
-
-Test case for the mutateParents() func
+Created on Wed Mar 21 15:51:18 2018
 
 @author: etcyl
 """
 
-import genetic
+def mutateAndPrint(evolver):
+    evolver.mutate()
+    printGenes(evolver)
 
-#Two lists of length 7 to act as two chromosomes
-X = [1, 2, 3, 4, 5, 6, 7]
-Y = [70, 80, 90, 1, 2, 3, 4]
-
-#Create morphological_evolver() class and two children A and B 
-m = genetic.morphological_evolver()
-(Z, T) = m.mutateParents(X, Y) #Put children A and B into variables Z and T
-print("parentA: ", X, "parentB: ", Y)
-print("childA: ", Z, "childB: ", T)
-
-#Set 2 out of 4 of the current population to children A and B
-m.current_chromosomes[0] = Z
-m.current_chromosomes[1] = T
+def printGenes(evolver):
+    for i in range(evolver.pop_size):
+        for j in range(evolver.num_genes):
+            print("Chromosome ", i, ", gene ", j, "value is: ", evolver.current_pop[i].getGene(j))

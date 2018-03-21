d35daaa5eac7d39d602153b4f85cdd58e8d970d8
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-03-21 16:05:39 -0700

    Update chromosome.py

--- chromosome.py
+++ chromosome.py
@@ -10,14 +10,15 @@ import random
 class chromosome():
     
     def __init__(self, length):
-        self.genes = [0]*length
-        for i in range(length):
+        self.accuruacy = 0 #Accuracy of an SVM, when it sequentially applies the genes to an image
+        self.genes = [0]*length #Storage for an individual
+        for i in range(length): #For all the genes in the list, 50% for the current element to be present or absent
             if(random.randint(0, 1) == 1):
-                self.setGene(i, 1)
+                self.setGene(i, 1) #Random value was 1 so the gene is present
             else:
-                self.setGene(i, 0)
+                self.setGene(i, 0) #Random value was 0 so the gene is absent
         
-    def toggleGene(self, index):
+    def toggleGene(self, index): #Turn the gene on or off, depending on the previous state (present or absent)
         if(self.genes[index] == 0):
             self.genes[index] = 1
         else:

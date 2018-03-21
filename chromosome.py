c3a21caf6b1d88318df80e672906a0799343f988
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-03-21 16:02:19 -0700

    Update chromosome.py

--- chromosome.py
+++ chromosome.py
@@ -5,13 +5,26 @@ Created on Wed Mar 21 15:05:42 2018
 @author: etcyl
 """
 
+import random
+
 class chromosome():
     
     def __init__(self, length):
         self.genes = [0]*length
+        for i in range(length):
+            if(random.randint(0, 1) == 1):
+                self.setGene(i, 1)
+            else:
+                self.setGene(i, 0)
         
-    def toggle_gene(self, index):
+    def toggleGene(self, index):
         if(self.genes[index] == 0):
             self.genes[index] = 1
         else:
             self.genes[index] = 0
+            
+    def getGene(self, index):
+        return self.genes[index]
+    
+    def setGene(self, index, value):
+        self.genes[index] = value

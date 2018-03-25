84ea6dae26340bf9c4cb7ed7a0a6eb791cbb3360
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-03-25 07:54:21 -0700

    Update chromosome.py

--- chromosome.py
+++ chromosome.py
@@ -2,6 +2,16 @@
 """
 Created on Wed Mar 21 15:05:42 2018
 
+Chromosomes are a list of genes, in this case each element in the list represents a morphologial operator:
+    
+    0: Erosion
+    1: Dilation (opposite of erosion)
+    2: Opening (erosion followed by dilation)
+    3: Closing (dilation followed by erosion)
+    4: Gradient (difference between dilation and erosion of an image)
+    5: Thinning
+    6: Thickening
+
 @author: etcyl
 """
 
@@ -10,7 +20,7 @@ import random
 class chromosome():
     
     def __init__(self, length):
-        self.accuracy = random.randint(0, 99) #Accuracy of an SVM, when it sequentially applies the genes to an image
+        self.accuracy = 0#random.randint(0, 99) #Accuracy of an SVM, when it sequentially applies the genes to an image
         self.genes = [0]*length #Storage for an individual
         for i in range(length): #For all the genes in the list, 50% for the current element to be present or absent
             if(random.randint(0, 1) == 1):
